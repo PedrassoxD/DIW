f5dc382cb962dc46212ada2724ddb390664f0e90
PedrassoxD <pedroperlab@gmail.com>
2019-11-28 14:31:07 +0100

    Nueva revision

--- Ejemplo_API_VUEJS/Ejemplo_Pedro/js.js
+++ Ejemplo_API_VUEJS/Ejemplo_Pedro/js.js
@@ -8,32 +8,44 @@ Vue.filter('format-thousands', function (value) {
     el: "#app",
     data: {
       textSearch: "",
-      countries: []
+      teams: []
     },
     computed: {
-       countriesFilter: function() {
+       teamsFilter: function() {
          var textSearch = this.textSearch;
-         return this.countries.filter(function(el) {
-           return el.name.toLowerCase().indexOf(textSearch.toLowerCase()) !== -1;
+         return this.teams.filter(function(el) {
+          return el.first_name.toLowerCase().indexOf(textSearch.toLowerCase()) !== -1;
          });
-       }
+       },
+       processedTeams() {
+        let teamsImg = this.teams;
+  
+        // Add image_url attribute
+        // Recorremos los equipos y a cada uno de ellos le añadimos la url del escudo de su equipo
+          teamsImg.forEach(team => {
+            team.image_url = "http://i.cdn.turner.com/nba/nba/.element/img/1.0/teamsites/logos/teamlogos_500x500/"+team.abbreviation.toLowerCase()+".png";      
+          });
+  
+        return teamsImg;
+      }
     },
     created: function() {
       var that = this;
       axios({
         "method":"GET",
-        "url":"https://free-nba.p.rapidapi.com/teams",
+        "url":"https://free-nba.p.rapidapi.com/players",
         "headers":{
         "content-type":"application/octet-stream",
         "x-rapidapi-host":"free-nba.p.rapidapi.com",
         "x-rapidapi-key":"3f674b37e8msh4a8932e8d199809p11be4bjsn91183b9afe86"
         },"params":{
-        "page":"0"
+        "page":"0",
+        "per_page":"25"
         }
         })
         .then((response)=>{
-            that.countries = response.data.data;
-          console.log(response.data)
+            that.teams = response.data.data;
+          console.log(response.data.data)
         })
         .catch((error)=>{
           console.log(error)
